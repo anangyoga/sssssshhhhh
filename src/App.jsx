7d7e5479f78db67a96345c1f4135e11eb4d46f2a
anangyoga <aharwinto@gmail.com>
2023-05-19 22:51:12 +0700

    setup vite & tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <p className="text-lg text-green-600 bg-gray-100 px-2 py-1 text-center">sssssssssshhhhhhhhhhhhhhhhhhhhhhhhhhhh</p>
+    </>
+  );
+};
+
+export default App;
