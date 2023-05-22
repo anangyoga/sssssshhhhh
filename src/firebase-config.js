ff82b3457a98c94558e0af289ef159e9b813d784
anangyoga <aharwinto@gmail.com>
2023-05-22 10:43:27 +0700

    fix: firebase & cookies

---
+++ src/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaz7CDlJd7XlaXjM5VAmHyR1xRR8h5k7A",
+  authDomain: "sssssshhhhhh-1b38a.firebaseapp.com",
+  projectId: "sssssshhhhhh-1b38a",
+  storageBucket: "sssssshhhhhh-1b38a.appspot.com",
+  messagingSenderId: "231666203997",
+  appId: "1:231666203997:web:79aaa8a6d3a70ccae5fcb4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
