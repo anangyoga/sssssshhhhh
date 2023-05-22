41b935a6f62f3f1a8623300002cf7e5a8baf39c4
anangyoga <aharwinto@gmail.com>
2023-05-22 10:51:19 +0700

    fix: refactored login auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import Dashboard from "./components/Dashboard";
 import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
-import { auth, provider } from "./firebase-config";
-import { signInWithPopup } from "firebase/auth";
 import Cookies from "universal-cookie";
 
 const cookies = new Cookies();
@@ -11,27 +9,9 @@ const cookies = new Cookies();
 const App = () => {
   const [isAuth, setIsAuth] = useState(cookies.get("authentication-token"));
 
-  const signInWithGoogle = async () => {
-    try {
-      const result = await signInWithPopup(auth, provider);
-      cookies.set("authentication-token", result.user.refreshToken);
-      setIsAuth(true);
-
-      console.log("clicked log");
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const signOut = async () => {
-    await signOut(auth);
-    console.log("clicked?");
-    cookies.remove("authentication-token");
-  };
-
   return (
     <main>
-      <Navbar signInWithGoogle={signInWithGoogle} signOut={signOut} setIsAuth={setIsAuth} />
+      <Navbar setIsAuth={setIsAuth} />
       {!isAuth ? <Homepage /> : <Dashboard />}
     </main>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,20 @@
-const Navbar = ({ signInWithGoogle }) => {
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
+
+const Navbar = ({ setIsAuth }) => {
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      cookies.set("authentication-token", result.user.refreshToken);
+      setIsAuth(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <nav className="flex sm:justify-end justify-center bg-green-400 py-2">
       <div className="mr-10 flex gap-x-5 text-sm text-white font-semibold">
