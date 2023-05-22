94742d09197902b368f5a18329ce1376fc7ed266
anangyoga <aharwinto@gmail.com>
2023-05-22 10:44:33 +0700

    feat: login auth with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import Dashboard from "./components/Dashboard";
 import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
+import { auth, provider } from "./firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 const App = () => {
+  const [isAuth, setIsAuth] = useState(cookies.get("authentication-token"));
+
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      cookies.set("authentication-token", result.user.refreshToken);
+      setIsAuth(true);
+
+      console.log("clicked log");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const signOut = async () => {
+    await signOut(auth);
+    console.log("clicked?");
+    cookies.remove("authentication-token");
+  };
+
   return (
     <main>
-      <Navbar />
-      <Homepage />
-      <Dashboard />
+      <Navbar signInWithGoogle={signInWithGoogle} signOut={signOut} setIsAuth={setIsAuth} />
+      {!isAuth ? <Homepage /> : <Dashboard />}
     </main>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-const Navbar = () => {
+const Navbar = ({ signInWithGoogle }) => {
   return (
     <nav className="flex sm:justify-end justify-center bg-green-400 py-2">
       <div className="mr-10 flex gap-x-5 text-sm text-white font-semibold">
-        <button className="bg-white/40 rounded-sm px-2 py-1 hover:opacity-80">Log In</button>
-        <button className="bg-white/40 rounded-sm px-2 py-1 hover:opacity-80">Register</button>
+        <button onClick={signInWithGoogle} className="bg-white/40 rounded-sm px-2 py-1 hover:opacity-80">
+          Log In
+        </button>
       </div>
     </nav>
   );
