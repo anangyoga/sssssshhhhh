09e544d26eef10f142ca39f08f610ffdd6bb71f4
anangyoga <aharwinto@gmail.com>
2023-05-24 13:38:54 +0700

    feat: fetch data from firestore

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,21 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 import LogoIcon from "../assets/message.svg";
+import { db } from "../firebase-config";
+import { getDocs, collection } from "firebase/firestore";
 
 const Header = () => {
+  const [dataMessages, setDataMessages] = useState([]);
+  const databaseRef = collection(db, "messages");
+
+  useEffect(() => {
+    const fetchMessages = async () => {
+      const messages = await getDocs(databaseRef);
+      setDataMessages(messages.docs.map((message) => ({ ...message.data(), id: message.id })));
+    };
+
+    fetchMessages();
+  }, []);
   return (
     <>
       <section className="flex flex-col gap-y-6 max-w-xl py-6 sm:mx-auto bg-gray-100 shadow-md rounded-sm my-3 mx-2">
@@ -9,8 +24,8 @@ const Header = () => {
           <p className="text-green-600 text-lg font-semibold">Send Anonymous Message!</p>
         </div>
         <div className="flex flex-col items-center">
-          <form className="flex flex-col gap-y-2 w-4/5 text-green-600">
-            <input type="text" placeholder="write your message..." className="p-2 h-8 rounded-sm  border-none focus:outline-none" />
+          <form className="flex flex-col gap-y-2 w-4/5  text-green-600">
+            <input type="text" placeholder="Write your message..." className="p-2 h-8 rounded-sm  border-none focus:outline-none" />
             <button type="submit" className="bg-green-500 text-white py-1 rounded-sm font-semibold">
               Send Message
             </button>
@@ -20,17 +35,11 @@ const Header = () => {
       <section className="flex flex-col gap-y-6 max-w-xl py-6 sm:mx-auto">
         <h2 className="text-lg font-semibold">Anonymous Messages for Fulan</h2>
         <div className="flex flex-col gap-y-4 bg-gray-50 shadow-md rounded-sm py-3 px-2">
-          <div className="bg-white px-2 py-3 rounded-sm shadow-sm">
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse libero autem earum dolor corrupti, quaerat asperiores obcaecati dolorum harum veritatis voluptatibus ab, illum officiis?</p>
-          </div>
-          <div className="bg-white px-2 py-3 rounded-sm shadow-sm">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur error deserunt distinctio.</p>
-          </div>
-          <div className="bg-white px-2 py-3 rounded-sm shadow-sm">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet totam rem excepturi quam facilis ullam pariatur voluptates. Perspiciatis excepturi repellat aperiam alias sequi sapiente expedita omnis quam vitae repellendus?
-            </p>
-          </div>
+          {dataMessages.map((data) => (
+            <div key={data.id} className="bg-white px-2 py-3 rounded-sm shadow-sm">
+              <p>{data.message}</p>
+            </div>
+          ))}
         </div>
       </section>
     </>
